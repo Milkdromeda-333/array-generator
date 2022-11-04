55d7f12146a1f49a9940ea5967968a4da2a312e7
Anjanique Mackey <anjaniquemackey@gmail.com>
2022-11-04 06:35:10 -0700

    completed mvp

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # array-generator
+
+This project was made because I needed to manually make a not-very-small array one too many times. 
+I decided to make this in vanilla javascript to dust off my ol' JS skills after learning React for so long.
+I'm very proud of this, and I plan on making more vanilla javascript mini projects and tools this way.
+
+With this project I:
+
+- Practiced determination. I forgot how to do not-React javascript. It was a long night.
+- Learned to delete a git remote (because i added git to the wrong folder🙃)
+- Learned that codesandbox is kinda crappy.
+
+To be done:
+[] add animation styling to the copy button after its clicked
+[]add themes lol
+[] edit array ?
+[] add button to clear array, and edit (and save)
+[] responsiveness
---
+++ index.js
@@ -0,0 +1,55 @@
+// VARIABLES
+
+const submitBtn = document.querySelector("#submit-btn");
+const input = document.querySelector("input");
+const canvas = document.querySelector("#canvas");
+const copyBtn = document.querySelector("#copy-btn");
+const userArr = [];
+
+// FUNCTIONS
+
+// handles submitting the user input
+function submit() {
+    const element = input.value;
+    userArr.push(element);
+    let arr = document.createElement("p");
+    arr.className = "generated-arr";
+    arr.innerHTML = `[${userArr.join(", ")}]`;
+    arr.style.fontSize = "1.5rem";
+    canvas.lastChild && canvas.removeChild(canvas.lastChild);
+    canvas.appendChild(arr);
+    input.value = "";
+}
+
+// submits users input if they hit enter
+document.addEventListener("keyup", (e) => {
+    if (e.code === "Enter") {
+        submit();
+    }
+});
+
+// submits users input
+submitBtn.addEventListener("click", submit);
+
+
+copyBtn.addEventListener("click", () => {
+    if (canvas.textContent === "Nothing here to copy...") {
+        return;
+    }
+    if (canvas.lastChild) {
+        canvas.lastChild.style.backgroundColor = "black";
+        canvas.lastChild.style.color = "white";
+        canvas.lastChild.style.padding = "0";
+        copyBtn.classList.add("copy-btn-copied");
+        navigator.clipboard.writeText(canvas.lastChild.textContent);
+
+        // removes the copy notif after 2 secs
+        setTimeout(() => {
+            copyBtn.classList.remove("copy-btn-copied");
+        }, 2000);
+    } else {
+        canvas.textContent = "Nothing here to copy...";
+        return;
+    }
+
+});
